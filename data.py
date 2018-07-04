3aac39013fa8e07f211ad57e7b3451f3bc5a98d9
Volodymyr Miroshnyk <vmiroshnik301290@gmail.com>
2018-07-04 16:37:25 +0300

    minor changes

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ wb = xl.load_workbook(r'2016_Черкаська обл..xlsx')
 sheet_names = wb.get_sheet_names()
 
 
-sheet = wb.get_sheet_by_name(sheet_names[2])
+sheet = wb.get_sheet_by_name(sheet_names[5])
 
 
 idx_cells = (('B4', 'Y34'), ('B40', 'Y70'), ('B76', 'Y106'), ('B112', 'Y142'),
@@ -15,10 +15,15 @@ cons = 'base'
 i = 0
 while cons:
     cons = sheet['A' + str(1 + i * 36)].value
-    i += 1
-    cons_r = sheet['AC' + str(2 + i * 36)].value
-    print(cons_r)
 
+    if cons:
+        data = sheet['B' + str(4 + i * 36):'Y' + str(34 + i * 36)]
+
+    cons_r = sheet['AC' + str(2 + i * 36)].value
+    print(cons)
+    if cons_r:
+        print(cons_r)
+    i += 1
 
 # for sheet in wb:
 #     for idx in idx_cells:
