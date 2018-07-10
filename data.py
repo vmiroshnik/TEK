890e26c58fcfd8f4b461d5fef9aa0106a62ab79f
Volodymyr Miroshnyk <vmiroshnik301290@gmail.com>
2018-07-10 12:32:28 +0300

    Row-wise analysis

--- data.py
+++ data.py
@@ -2,56 +2,34 @@ import os
 
 import openpyxl as xl
 
-wb = xl.load_workbook(r'2016_Черкаська обл..xlsx')
+source_filename = r'2017_Черкаська обл..xlsx'
+
+wb = xl.load_workbook(source_filename)
 
 sheet_names = wb.get_sheet_names()
 
 idx_cells = (('B4', 'Y34'), ('B40', 'Y70'), ('B76', 'Y106'), ('B112', 'Y142'),
              ('B148', 'Y178'), ('B184', 'Y214'), ('AD184', 'BA214'))
 
-base_path = r'2016/'
+base_path = source_filename.split('_')[0] + '/'
 for month in sheet_names:
     month_path = base_path + month + '/'
     try:
-        os.mkdir(month_path)
+        os.makedirs(month_path)
     except FileExistsError:
         pass
 
     sheet = wb.get_sheet_by_name(month)
-    m = 0
-    cons = 'base'
 
-    while cons:
-        cons = sheet['A' + str(1 + m * 36)].value
-        tmp = []
-        if cons:
-            data = sheet['B' + str(4 + m * 36):'Y' + str(34 + m * 36)]
-            for i in range(len(data)):
-                for j in range(len(data[i])):
-                    tmp.append(data[i][j].value)
+    for row in sheet.iter_rows(min_row=1, max_col=55, max_row=322):
+        if isinstance(row[0].value, str) and 'Споживач' in row[0].value:
+            cons = row[0].value.split('"')[1]
+            file_path = month_path + cons + '.txt'
+        if isinstance(row[0].value, int) and isinstance(row[1].value, int) and isinstance(row[10].value, int):
+            data = [row[i].value if isinstance(row[i].value, int) else 0 for i in range(1, 25)]
+            if isinstance(row[29].value, int):
+                data_r = [row[i].value if isinstance(row[i].value, int) else 0 for i in range(29, 53)]
+                data = list(map(sum, zip(data, data_r)))
+            with open(file_path, 'a') as f:
+                f.write(','.join(map(str, data)) + '\n')
 
-        cons_r = sheet['AC' + str(2 + m * 36)].value
-        print(cons)
-        tmp_r = []
-        res = []
-        if cons_r:
-            data = sheet['AD' + str(4 + m * 36):'BA' + str(34 + m * 36)]
-            for i in range(len(data)):
-                for j in range(len(data[i])):
-                    tmp_r.append(data[i][j].value)
-
-            for s in zip(tmp, tmp_r):
-                try:
-                    res.append(sum(s))
-                except TypeError:
-                    res.append('')
-            print(cons_r)
-        else:
-            res.extend(tmp)
-
-        if cons:
-            file_path = month_path + cons.split('"')[1] + '.txt'
-
-            with open(file_path, 'w') as f:
-                f.write(', '.join(map(str, res)))
-        m += 1
